577a5e75ced257c556f0fae5525d8ca1fe1dc1bc
Alef Francisco da Silva <alefregistro@hotmail.com>
2023-03-13 17:18:06 -0300

    Inicio da arquitetura de pastas usando a ITCSS

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="contain">
-          <p>Sobre</p>
-        </div>
-        <div className="contain2">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Aprendendo React
-          </a>
-        </div>
-      </header>
-    </div>
+    <a href="#to-do" className="action">
+      Sobre
+    </a>
   );
 }
 
